05a5eee183055276158f76560e83559b01d8310b
Kenisha Randhawa <kenissharandhawa@gmail.com>
2026-01-23 13:58:19 +0530

    js session 1

---
+++ JS-Day-4/script.js
@@ -0,0 +1,11 @@
+let user ={
+    FullName: "Kenisha Randhawa",
+    address: "Gurugram",
+    mobileno: 3456463258,
+    favcolor: ["Purple", "Green", "Silver"]
+}
+console.log(user.FullName, user.address, user.favcolor[0]);
+//we can access the individual value by targeting key with variable name.
+
+let date = new Date()
+console.log(date);
